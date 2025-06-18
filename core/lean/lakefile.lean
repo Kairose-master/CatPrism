7b8657e44212664351c8d88e29029e33a1c0568a
Kairose_master <77557438+Kairose-master@users.noreply.github.com>
2025-06-19 03:30:32 +0900

    lock mathlib

--- core/lean/lakefile.lean
+++ core/lean/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package CatPrism
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "main"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "53a6dc9"
 
 @[default_target]
 lean_lib Core
