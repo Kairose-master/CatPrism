efc1085cbc655951b0cff2fafc1bbeb3dceac334
Kairose_master <77557438+Kairose-master@users.noreply.github.com>
2025-06-21 13:06:01 +0900

    Core.lean 업데이트

--- core/lean/Core.lean
+++ core/lean/Core.lean
@@ -5,31 +5,35 @@ import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 import Mathlib.Tactic
 
 open CategoryTheory
-universe u
+universe u v
+
+class CatPrismCategory (C : Type u) extends Category.{u, v} C
 
 namespace CatPrism.Core
 
-class CatPrismCategory (C : Type u) extends Category C
-
-class HasPhase {C : Type u} [CatPrismCategory C] : Type (u+2) where
+@[class] structure HasPhase (C : Type u) [CatPrismCategory C] :
+    Type (max u (v+1)) where
   phase     : {A B : C} → (A ⟶ B) → ℝ
-  phase_arg : ∀ {A B : C} (f : A ⟶ B), ‖phase f‖ ≤ Real.pi
+  phase_arg : {A B : C} → (f : A ⟶ B) → ‖phase f‖ ≤ Real.pi
 
-@[inline] def PhaseDist {C : Type u} [CatPrismCategory C] [HasPhase (C := C)]
+@[inline] def PhaseDist
+    {C : Type u} [CatPrismCategory C] [h : HasPhase C]
     {A B : C} (f g : A ⟶ B) : ℝ :=
-  ‖HasPhase.phase f - HasPhase.phase g‖
+  ‖h.phase f - h.phase g‖
 
-class HasLength {C : Type u} [CatPrismCategory C] : Type (u+2) where
+@[class] structure HasLength (C : Type u) [CatPrismCategory C] :
+    Type (max u (v+1)) where
   length     : {A B : C} → (A ⟶ B) → ℝ
-  len_nonneg : ∀ {A B : C} (f : A ⟶ B), 0 ≤ length f
+  len_nonneg : {A B : C} → (f : A ⟶ B) → 0 ≤ length f
 
-@[inline] def LengthDist {C : Type u} [CatPrismCategory C] [HasLength (C := C)]
+@[inline] def LengthDist
+    {C : Type u} [CatPrismCategory C] [h : HasLength C]
     {A B : C} (f g : A ⟶ B) : ℝ :=
-  ‖HasLength.length f - HasLength.length g‖
+  ‖h.length f - h.length g‖
 
-@[inline] def Δzero {C : Type u} [CatPrismCategory C] {A B : C}
-    (_f _g : A ⟶ B) : ℝ :=
-  0
+@[inline] def Δzero
+    {C : Type u} [CatPrismCategory C]
+    {A B : C} (_f _g : A ⟶ B) : ℝ := 0
 
 inductive UnitCat : Type
 | star
@@ -38,41 +42,19 @@ deriving DecidableEq, Inhabited
 open UnitCat
 
 instance : CatPrismCategory UnitCat where
-  Hom _ _ := PUnit
-  id _ := PUnit.unit
-  comp := by
-    intro _ _ _ _ _
-    exact PUnit.unit
-  id_comp := by
-    intro _ _ f
-    cases f
-    simp
-  comp_id := by
-    intro _ _ f
-    cases f
-    simp
-  assoc := by
-    intro _ _ _ _ f g h
-    cases f
-    cases g
-    cases h
-    simp
+  Hom      := fun _ _ => PUnit
+  id       := fun _ => PUnit.unit
+  comp     := fun _ _ _ _ _ => PUnit.unit
+  id_comp  := by intro _ _ f; cases f; rfl
+  comp_id  := by intro _ _ f; cases f; rfl
+  assoc    := by intro _ _ _ _ f g h; cases f; cases g; cases h; rfl
 
-instance : HasPhase (C := UnitCat) where
-  phase := by
-    intro _ _ _
-    exact 0
-  phase_arg := by
-    intro _ _ _
-    simp [Real.pi_pos.le]
+instance : HasPhase UnitCat where
+  phase     := fun {_ _} _ => 0
+  phase_arg := fun {_ _} _ => by simp [Real.pi_pos.le]
 
-instance : HasLength (C := UnitCat) where
-  length := by
-    intro _ _ _
-    exact 0
-  len_nonneg := by
-    intro _ _ _
-    simp
+instance : HasLength UnitCat where
+  length     := fun {_ _} _ => 0
+  len_nonneg := fun {_ _} _ => by simp
 
 end CatPrism.Core
-
